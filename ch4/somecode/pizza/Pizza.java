638a3522f22f44a7d4f2613204b658b95258344c
ankanchanda <ankan.chanda74@gmail.com>
2025-05-11 23:05:34 +0530

    print formatting changes

--- ch4/somecode/pizza/Pizza.java
+++ ch4/somecode/pizza/Pizza.java
@@ -9,28 +9,33 @@ public abstract class Pizza {
 	public String sauce;
 	public List<String> toppings = new ArrayList<String>();
 
+	public void prepare() {
+		System.out.println("Prepare " + name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding sauce...");
+		System.out.println("Adding toppings: ");
+		for (String topping : toppings) {
+			System.out.println("   " + topping);
+		}
+	}
+  
+	public void bake() {
+		System.out.println("Bake for 25 minutes at 350");
+	}
+ 
+	public void cut() {
+		System.out.println("Cut the pizza into diagonal slices");
+	}
+  
+	public void box() {
+		System.out.println("Place pizza in official PizzaStore box");
+	}
+ 
 	public String getName() {
 		return name;
 	}
 
-	public void prepare() {
-		System.out.println("Preparing " + name);
-	}
-
-	public void bake() {
-		System.out.println("Baking " + name);
-	}
-
-	public void cut() {
-		System.out.println("Cutting " + name);
-	}
-
-	public void box() {
-		System.out.println("Boxing " + name);
-	}
-
 	public String toString() {
-		// code to display pizza name and ingredients
 		StringBuffer display = new StringBuffer();
 		display.append("---- " + name + " ----\n");
 		display.append(dough + "\n");
